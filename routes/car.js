10266bee012ed757202c36cc0f3a09a7b2aaafa5
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-04-07 09:55:09 -0500

    add cars (post route)

--- routes/car.js
+++ routes/car.js
@@ -15,3 +15,23 @@ router.get('/:id', async (req, res) => {
   if (!car) return res.status(404).send('Car not found')
   res.send(car)
 })
+
+router.post('/', [
+  check('company').isLength({min: 3}),
+  check('model').isLength({min: 3})
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()})
+  }
+  const car = new Car({
+    company: req.body.company,
+    model: req.body.model,
+    year: req.body.year,
+    sold: req.body.sold,
+    price: req.body.price,
+    extras: req.body.extras
+  })
+  const result = await car.save()
+  res.status(201).send(result)
+})
