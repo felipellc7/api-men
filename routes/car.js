3e735018b35226864fe5f45e8147863899859369
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-04-07 09:40:55 -0500

    add list car and id (get routes)

--- routes/car.js
+++ routes/car.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const Car = require('../models/car')
 const router = express.Router()
 
 const { check, validationResult } = require('express-validator')
 
-router.get('/list', (req, res) => {
-  res.send('router get connect')
+router.get('/', async (req, res) => {
+  const cars = await Car.find()
+  res.send(cars)
+})
+
+router.get('/:id', async (req, res) => {
+  const car = await Car.findById(req.params.id)
+  if (!car) return res.status(404).send('Car not found')
+  res.send(car)
 })
